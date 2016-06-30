ff3753f7889343caa3ffd3f7c07818b96c4323d0
sowjanya846 <sowjanya.pothireddi@innroad.com>
2016-06-30 15:28:54 +0530

    abcded

--- InnRoad_DataDrivenFrameWork/Run.bat
+++ InnRoad_DataDrivenFrameWork/Run.bat
@@ -1,1 +1,1 @@
-java -cp InnRoad_DataDrivenFrameWork\bin;InnRoad_DataDrivenFrameWork\lib\* org.testng.TestNG testng.xml
+java -cp InnRoad_DataDrivenFrameWork\bin;InnRoad_DataDrivenFrameWork\lib\* org.testng.TestNG test-output\testng-results.xml
